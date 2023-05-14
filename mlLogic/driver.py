75720fa1e98c14f1a4f8399ebe723089f7ce4a65
KunjPathak12 <kunjpathak1212@gmail.com>
2023-05-14 20:45:11 +0530

    commit validation that feature working but not live

--- mlLogic/driver.py
+++ mlLogic/driver.py
@@ -1,3 +1,13 @@
+from captGendeploy.settings import BASE_DIR
 from functionFile import *
-myList = imgPredictStep(["pictures/Image1.png","pictures/Image2.png", "pictures/Image3.png"])
-print(*myList, sep="\n")
+import os,shutil
+imgRoot = os.path.join(BASE_DIR, 'media','images')
+folder_dir = imgRoot
+imgList = []
+for images in os.listdir(folder_dir):
+    imgList.append(images)
+img = str(imgList[0])
+finalImg = imgRoot+"\\"+img
+caption = imgPredictStep([finalImg])
+print(*caption)
+shutil.rmtree(folder_dir)
